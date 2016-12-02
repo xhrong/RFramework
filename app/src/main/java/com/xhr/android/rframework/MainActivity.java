9b45b14c7fff6a916799b94cb7e34c73f088f96d
xhrong <rxh_et@163.com>
2016-12-02 17:23:13 +0800

    添加DeviceUtils和NetworkUtils

--- app/src/main/java/com/xhr/android/rframework/MainActivity.java
+++ app/src/main/java/com/xhr/android/rframework/MainActivity.java
@@ -1,34 +1,11 @@
 package com.xhr.android.rframework;
 
-import android.app.ActivityManager;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.googlecode.tesseract.android.TessBaseAPI;
-import com.xhr.and.rframework.utils.HttpUtils;
-import com.xhr.and.rframework.utils.ImageUtils;
-import com.xhr.and.rframework.utils.LogUtils;
-
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-
-import jp.co.cyberagent.android.gpuimage.GPUImage;
-import jp.co.cyberagent.android.gpuimage.GPUImage3x3ConvolutionFilter;
-import jp.co.cyberagent.android.gpuimage.GPUImageCGAColorspaceFilter;
-import jp.co.cyberagent.android.gpuimage.GPUImageColorBurnBlendFilter;
-import jp.co.cyberagent.android.gpuimage.GPUImageFilter;
-import jp.co.cyberagent.android.gpuimage.GPUImageSharpenFilter;
-import jp.co.cyberagent.android.gpuimage.GPUImageSobelEdgeDetection;
-import jp.co.cyberagent.android.gpuimage.GPUImageTwoPassFilter;
-import jp.co.cyberagent.android.gpuimage.GPUImageView;
-
+import com.xhr.and.rframework.utils.DeviceUtils;
+import com.xhr.and.rframework.utils.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,24 +16,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        NetworkUtils.enableWifi(this,true);
 
-        try {
-            String imgPath = Environment.getExternalStorageDirectory() + "/ori.jpg";
-            String imgOutPath = Environment.getExternalStorageDirectory() + "/ori33.jpg";
+        Log.e("getIPAddress", NetworkUtils.isInternetAvailable()+"");
 
-
-
-            BitmapFactory.Options options = new BitmapFactory.Options();
-
-            options.inScaled = false;
-            Bitmap bitmap = BitmapFactory.decodeFile(imgPath,options);
-
-            Log.e("TTTTT",bitmap.getByteCount()+"");
-          Bitmap    bitmapa = ImageUtils.rotateImage(90, bitmap);
-            Log.e("TTTTT",bitmap.getByteCount()+"");
-            ImageUtils.saveImage(bitmapa, imgOutPath, 100);
-        } catch (Exception e) {
-            Log.e("EEE", e.getMessage());
-        }
+//        Log.e("getDomainAddress", NetworkUtils.getIPAddress(true));
+//
+//        Log.e("getNetworkType", NetworkUtils.getNetworkType(this).toString());
+//
+//        Log.e("getMacAddress", DeviceUtils.getMacAddress(this));
+//
+//        Log.e("getPhoneStatus",DeviceUtils.getPhoneStatus(this));
+//
+//
+//        try {
+//            String imgPath = Environment.getExternalStorageDirectory() + "/ori.jpg";
+//            String imgOutPath = Environment.getExternalStorageDirectory() + "/ori33.jpg";
+//
+//
+//
+//            BitmapFactory.Options options = new BitmapFactory.Options();
+//
+//            options.inScaled = false;
+//            Bitmap bitmap = BitmapFactory.decodeFile(imgPath,options);
+//
+//            Log.e("TTTTT",bitmap.getByteCount()+"");
+//          Bitmap    bitmapa = ImageUtils.rotateImage(90, bitmap);
+//            Log.e("TTTTT",bitmap.getByteCount()+"");
+//            ImageUtils.saveImage(bitmapa, imgOutPath, 100);
+//        } catch (Exception e) {
+//            Log.e("EEE", e.getMessage());
+//        }
     }
 }
---
+++ rframework/src/main/java/com/xhr/and/rframework/utils/DeviceUtils.java
@@ -0,0 +1,427 @@
+package com.xhr.and.rframework.utils;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by xhrong on 2016/12/2.
+ */
+public class DeviceUtils {
+
+    /**
+     * 判断设备是否root
+     *
+     * @return the boolean{@code true}: 是<br>{@code false}: 否
+     */
+    public static boolean isDeviceRoot() {
+        String su = "su";
+        String[] locations = {"/system/bin/", "/system/xbin/", "/sbin/", "/system/sd/xbin/", "/system/bin/failsafe/",
+                "/data/local/xbin/", "/data/local/bin/", "/data/local/"};
+        for (String location : locations) {
+            if (new File(location + su).exists()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 获取设备系统版本号
+     *
+     * @return 设备系统版本号
+     */
+    public static int getSDKVersion() {
+        return android.os.Build.VERSION.SDK_INT;
+    }
+
+
+    /**
+     * 获取设备AndroidID
+     *
+     * @param context 上下文
+     * @return AndroidID
+     */
+    @SuppressLint("HardwareIds")
+    public static String getAndroidID(Context context) {
+        return Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+    }
+
+    /**
+     * 获取设备MAC地址
+     * <p>需添加权限 {@code <uses-permission android:name="android.permission.ACCESS_WIFI_STATE"/>}</p>
+     * <p>需添加权限 {@code <uses-permission android:name="android.permission.INTERNET"/>}</p>
+     *
+     * @param context 上下文
+     * @return MAC地址
+     */
+    public static String getMacAddress(Context context) {
+        String macAddress = getMacAddressByWifiInfo(context);
+        if (!"02:00:00:00:00:00".equals(macAddress)) {
+            return macAddress;
+        }
+        macAddress = getMacAddressByNetworkInterface();
+        if (!"02:00:00:00:00:00".equals(macAddress)) {
+            return macAddress;
+        }
+        macAddress = getMacAddressByFile();
+        if (!"02:00:00:00:00:00".equals(macAddress)) {
+            return macAddress;
+        }
+        return "please open wifi";
+    }
+
+    /**
+     * 获取设备MAC地址
+     * <p>需添加权限 {@code <uses-permission android:name="android.permission.ACCESS_WIFI_STATE"/>}</p>
+     *
+     * @param context 上下文
+     * @return MAC地址
+     */
+    @SuppressLint("HardwareIds")
+    private static String getMacAddressByWifiInfo(Context context) {
+        try {
+            WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+            if (wifi != null) {
+                WifiInfo info = wifi.getConnectionInfo();
+                if (info != null) return info.getMacAddress();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "02:00:00:00:00:00";
+    }
+
+    /**
+     * 获取设备MAC地址
+     * <p>需添加权限 {@code <uses-permission android:name="android.permission.INTERNET"/>}</p>
+     *
+     * @return MAC地址
+     */
+    private static String getMacAddressByNetworkInterface() {
+        try {
+            List<NetworkInterface> nis = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface ni : nis) {
+                if (!ni.getName().equalsIgnoreCase("wlan0")) continue;
+                byte[] macBytes = ni.getHardwareAddress();
+                if (macBytes != null && macBytes.length > 0) {
+                    StringBuilder res1 = new StringBuilder();
+                    for (byte b : macBytes) {
+                        res1.append(String.format("%02x:", b));
+                    }
+                    return res1.deleteCharAt(res1.length() - 1).toString();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "02:00:00:00:00:00";
+    }
+
+    /**
+     * 获取设备MAC地址
+     *
+     * @return MAC地址
+     */
+    private static String getMacAddressByFile() {
+        InnerUtils.CommandResult result = InnerUtils.execCmd("getprop wifi.interface", false);
+        if (result.result == 0) {
+            String name = result.successMsg;
+            if (name != null) {
+                result = InnerUtils.execCmd("cat /sys/class/net/" + name + "/address", false);
+                if (result.result == 0) {
+                    if (result.successMsg != null) {
+                        return result.successMsg;
+                    }
+                }
+            }
+        }
+        return "02:00:00:00:00:00";
+    }
+
+    /**
+     * 获取设备厂商
+     * <p>如Xiaomi</p>
+     *
+     * @return 设备厂商
+     */
+    public static String getManufacturer() {
+        return Build.MANUFACTURER;
+    }
+
+    /**
+     * 获取设备型号
+     * <p>如MI2SC</p>
+     *
+     * @return 设备型号
+     */
+    public static String getModel() {
+        String model = Build.MODEL;
+        if (model != null) {
+            model = model.trim().replaceAll("\\s*", "");
+        } else {
+            model = "";
+        }
+        return model;
+    }
+
+
+
+    /**
+     * 判断设备是否是手机
+     *
+     * @param context 上下文
+     * @return {@code true}: 是<br>{@code false}: 否
+     */
+    public static boolean isPhone(Context context) {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        return tm != null && tm.getPhoneType() != TelephonyManager.PHONE_TYPE_NONE;
+    }
+
+    /**
+     * 获取IMEI码
+     * <p>需添加权限 {@code <uses-permission android:name="android.permission.READ_PHONE_STATE"/>}</p>
+     *
+     * @param context 上下文
+     * @return IMIE码
+     */
+    @SuppressLint("HardwareIds")
+    public static String getIMEI(Context context) {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        return tm != null ? tm.getDeviceId() : null;
+    }
+
+    /**
+     * 获取IMSI码
+     * <p>需添加权限 {@code <uses-permission android:name="android.permission.READ_PHONE_STATE"/>}</p>
+     *
+     * @param context 上下文
+     * @return IMIE码
+     */
+    @SuppressLint("HardwareIds")
+    public static String getIMSI(Context context) {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        return tm != null ? tm.getSubscriberId() : null;
+    }
+
+    /**
+     * 获取移动终端类型
+     *
+     * @param context 上下文
+     * @return 手机制式
+     * <ul>
+     * <li>{@link TelephonyManager#PHONE_TYPE_NONE } : 0 手机制式未知</li>
+     * <li>{@link TelephonyManager#PHONE_TYPE_GSM  } : 1 手机制式为GSM，移动和联通</li>
+     * <li>{@link TelephonyManager#PHONE_TYPE_CDMA } : 2 手机制式为CDMA，电信</li>
+     * <li>{@link TelephonyManager#PHONE_TYPE_SIP  } : 3</li>
+     * </ul>
+     */
+    public static int getPhoneType(Context context) {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        return tm != null ? tm.getPhoneType() : -1;
+    }
+
+    /**
+     * 判断sim卡是否准备好
+     *
+     * @return {@code true}: 是<br>{@code false}: 否
+     */
+    public static boolean isSimCardReady(Context context) {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        return tm != null && tm.getSimState() == TelephonyManager.SIM_STATE_READY;
+    }
+
+    /**
+     * 获取Sim卡运营商名称
+     * <p>中国移动、如中国联通、中国电信</p>
+     *
+     * @param context 上下文
+     * @return sim卡运营商名称
+     */
+    public static String getSimOperatorName(Context context) {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        return tm != null ? tm.getSimOperatorName() : null;
+    }
+
+    /**
+     * 获取Sim卡运营商名称
+     * <p>中国移动、如中国联通、中国电信</p>
+     *
+     * @param context 上下文
+     * @return 移动网络运营商名称
+     */
+    public static String getSimOperatorByMnc(Context context) {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        String operator = tm != null ? tm.getSimOperator() : null;
+        if (operator == null) return null;
+        switch (operator) {
+            case "46000":
+            case "46002":
+            case "46007":
+                return "中国移动";
+            case "46001":
+                return "中国联通";
+            case "46003":
+                return "中国电信";
+            default:
+                return operator;
+        }
+    }
+
+    /**
+     * 获取手机状态信息
+     * <p>需添加权限 {@code <uses-permission android:name="android.permission.READ_PHONE_STATE"/>}</p>
+     *
+     * @param context 上下文
+     * @return DeviceId(IMEI) = 99000311726612<br>
+     * DeviceSoftwareVersion = 00<br>
+     * Line1Number =<br>
+     * NetworkCountryIso = cn<br>
+     * NetworkOperator = 46003<br>
+     * NetworkOperatorName = 中国电信<br>
+     * NetworkType = 6<br>
+     * honeType = 2<br>
+     * SimCountryIso = cn<br>
+     * SimOperator = 46003<br>
+     * SimOperatorName = 中国电信<br>
+     * SimSerialNumber = 89860315045710604022<br>
+     * SimState = 5<br>
+     * SubscriberId(IMSI) = 460030419724900<br>
+     * VoiceMailNumber = *86<br>
+     */
+    public static String getPhoneStatus(Context context) {
+        TelephonyManager tm = (TelephonyManager) context
+                .getSystemService(Context.TELEPHONY_SERVICE);
+        String str = "";
+        str += "DeviceId(IMEI) = " + tm.getDeviceId() + "\n";
+        str += "DeviceSoftwareVersion = " + tm.getDeviceSoftwareVersion() + "\n";
+        str += "Line1Number = " + tm.getLine1Number() + "\n";
+        str += "NetworkCountryIso = " + tm.getNetworkCountryIso() + "\n";
+        str += "NetworkOperator = " + tm.getNetworkOperator() + "\n";
+        str += "NetworkOperatorName = " + tm.getNetworkOperatorName() + "\n";
+        str += "NetworkType = " + tm.getNetworkType() + "\n";
+        str += "PhoneType = " + tm.getPhoneType() + "\n";
+        str += "SimCountryIso = " + tm.getSimCountryIso() + "\n";
+        str += "SimOperator = " + tm.getSimOperator() + "\n";
+        str += "SimOperatorName = " + tm.getSimOperatorName() + "\n";
+        str += "SimSerialNumber = " + tm.getSimSerialNumber() + "\n";
+        str += "SimState = " + tm.getSimState() + "\n";
+        str += "SubscriberId(IMSI) = " + tm.getSubscriberId() + "\n";
+        str += "VoiceMailNumber = " + tm.getVoiceMailNumber() + "\n";
+        return str;
+    }
+
+    private static class InnerUtils {
+        /**
+         * 是否是在root下执行命令
+         *
+         * @param command 命令
+         * @param isRoot  是否需要root权限执行
+         * @return CommandResult
+         */
+        public static CommandResult execCmd(String command, boolean isRoot) {
+            return execCmd(new String[]{command}, isRoot, true);
+        }
+
+        /**
+         * 是否是在root下执行命令
+         *
+         * @param commands        命令数组
+         * @param isRoot          是否需要root权限执行
+         * @param isNeedResultMsg 是否需要结果消息
+         * @return CommandResult
+         */
+        public static CommandResult execCmd(String[] commands, boolean isRoot, boolean isNeedResultMsg) {
+            int result = -1;
+            if (commands == null || commands.length == 0) {
+                return new CommandResult(result, null, null);
+            }
+            Process process = null;
+            BufferedReader successResult = null;
+            BufferedReader errorResult = null;
+            StringBuilder successMsg = null;
+            StringBuilder errorMsg = null;
+            DataOutputStream os = null;
+            try {
+                process = Runtime.getRuntime().exec(isRoot ? "su" : "sh");
+                os = new DataOutputStream(process.getOutputStream());
+                for (String command : commands) {
+                    if (command == null) continue;
+                    os.write(command.getBytes());
+                    os.writeBytes("\n");
+                    os.flush();
+                }
+                os.writeBytes("exit\n");
+                os.flush();
+                result = process.waitFor();
+                if (isNeedResultMsg) {
+                    successMsg = new StringBuilder();
+                    errorMsg = new StringBuilder();
+                    successResult = new BufferedReader(new InputStreamReader(process.getInputStream(), "UTF-8"));
+                    errorResult = new BufferedReader(new InputStreamReader(process.getErrorStream(), "UTF-8"));
+                    String s;
+                    while ((s = successResult.readLine()) != null) {
+                        successMsg.append(s);
+                    }
+                    while ((s = errorResult.readLine()) != null) {
+                        errorMsg.append(s);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (os != null)
+                        os.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (process != null) {
+                    process.destroy();
+                }
+            }
+            return new CommandResult(
+                    result,
+                    successMsg == null ? null : successMsg.toString(),
+                    errorMsg == null ? null : errorMsg.toString()
+            );
+        }
+
+        /**
+         * 返回的命令结果
+         */
+        public static class CommandResult {
+            /**
+             * 结果码
+             **/
+            public int result;
+            /**
+             * 成功信息
+             **/
+            public String successMsg;
+            /**
+             * 错误信息
+             **/
+            public String errorMsg;
+
+            public CommandResult(int result, String successMsg, String errorMsg) {
+                this.result = result;
+                this.successMsg = successMsg;
+                this.errorMsg = errorMsg;
+            }
+        }
+    }
+}
